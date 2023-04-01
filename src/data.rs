f7d1364c5b462ac0f7ac31af6cfd53884594d081
Patrick Elsen <pelsen@xfbs.net>
2023-04-01 14:35:26 +0200

    Updates

--- src/data.rs
+++ src/data.rs
@@ -8,36 +8,30 @@ use std::rc::Rc;
 /// are not limited to, synchronization for multithreaded access and the ability to make copies of
 /// the data.
 ///
-/// The `Data` trait is generic over the type of the inner value of the shared data, `T`. It also
-/// defines an associated type `Storage`, which represents the specific type that the inner value of
-/// the shared data is wrapped in.
+/// The `Data` trait is generic over the type of the inner value of the shared data, `T`.
+/// Implementers of this trait must provide methods to immutable access, and may provide methods to
+/// mutably access the data.
 pub trait Data<T> {
-    /// Underlying storage of the data.
+    /// Create some new data.
     ///
-    /// The type of this depends on what storage mechanism is being used.
+    /// # Example
     ///
-    /// Usually, this is some wrapper type such as [`Arc<T>`] or [`Rc<T>`] that can be cheaply
-    /// cloned.
-    type Storage;
-
-    /// Create some new wrapped data.
+    /// Thread-safe shared data using [`Arc<T>`]:
     ///
-    /// # Examples
+    /// ```
+    /// use imstr::data::Data;
+    /// use std::sync::Arc;
     ///
-    /// Create new shared, threadsafe data:
-    ///
-    /// ```rust
-    /// use imstr::data::{Data, Threadsafe};
-    ///
-    /// let threadsafe = Threadsafe::new("Hello".to_string());
+    /// let data = <Arc<_> as Data<_>>::new(15);
     /// ```
     ///
-    /// Create new shared, local (non-threadsafe) data:
+    /// Non thread-safe shared data using [`Rc<T>`]:
     ///
-    /// ```rust
-    /// use imstr::data::{Data, Local};
+    /// ```
+    /// use imstr::data::Data;
+    /// use std::rc::Rc;
     ///
-    /// let local = Local::new("Hello".to_string());
+    /// let data = <Rc<_> as Data<_>>::new(15);
     /// ```
     fn new(value: T) -> Self;
 
@@ -45,10 +39,23 @@ pub trait Data<T> {
     ///
     /// # Example
     ///
-    /// ```rust
-    /// use imstr::data::{Data, Local};
+    /// Thread-safe shared data using [`Arc<T>`]:
     ///
-    /// let data = Local::new(15);
+    /// ```rust
+    /// use imstr::data::Data;
+    /// use std::sync::Arc;
+    ///
+    /// let data = Arc::new(15);
+    /// assert_eq!(data.get(), &15);
+    /// ```
+    ///
+    /// Non thread-safe shared data using [`Rc<T>`]:
+    ///
+    /// ```rust
+    /// use imstr::data::Data;
+    /// use std::rc::Rc;
+    ///
+    /// let data = Rc::new(15);
     /// assert_eq!(data.get(), &15);
     /// ```
     fn get(&self) -> &T;
@@ -63,19 +70,19 @@ pub trait Data<T> {
     /// possible for shared data. However, in the case that it is possible, this is a good
     /// optimisation because it means the data does not have to be copied in order to mutate it.
     ///
-    /// For example, [`Arc`](std::sync::Arc), which backs the [`Threadsafe`] data type,  allows
-    /// mutating the data if and only if there is only a single reference to it alive (the
-    /// reference count is 1). The same is true for [`Rc`](std::rc::Rc) which backs the [`Local`]
-    /// data type.
+    /// For example, [`Arc`](std::sync::Arc), allows mutating the data if and only if there is only
+    /// a single reference to it alive (the reference count is 1). The same is true for
+    /// [`Rc`](std::rc::Rc).
     ///
     /// # Examples
     ///
-    /// Mutating some [`Local`] data:
+    /// Mutating thread-safe data wrapped in [`Arc<T>`]:
     ///
     /// ```rust
-    /// use imstr::data::{Data, Local};
+    /// use imstr::data::Data;
+    /// use std::sync::Arc;
     ///
-    /// let mut data = Local::new(15);
+    /// let mut data = Arc::new(15);
     /// if let Some(mut data) = data.get_mut() {
     ///     *data += 1;
     /// }
@@ -83,122 +90,71 @@ pub trait Data<T> {
     /// assert_eq!(data.get(), &16);
     /// ```
     ///
-    /// Mutating some [`Threadsafe`] data:
+    /// Mutating non-thread-safe data wrapped in [`Rc<T>`]:
     ///
     /// ```rust
-    /// use imstr::data::{Data, Threadsafe};
+    /// use imstr::data::Data;
+    /// use std::rc::Rc;
     ///
-    /// let mut data = Threadsafe::new(15);
+    /// let mut data = Rc::new(15);
     /// if let Some(mut data) = data.get_mut() {
     ///     *data += 1;
     /// }
     ///
     /// assert_eq!(data.get(), &16);
     /// ```
-    fn get_mut(&mut self) -> Option<&mut T>;
-
-    /// Returns a clone of the raw, inner storage.
-    fn raw(&self) -> Self::Storage;
+    fn get_mut(&mut self) -> Option<&mut T> {
+        None
+    }
 }
 
-/// Shared data, thread-safe.
-///
-/// The `Sync<T>` type represents shared data that can be safely accessed from multiple threads
-/// concurrently. It is backed by an `Arc<T>`, which is an atomic reference-counted pointer that
-/// allows multiple ownership of its inner value.
-///
-/// Since the `Sync<T>` type is backed by an `Arc<T>`, the inner value of the shared data can be
-/// cloned efficiently using the `clone()` method. The `Sync<T>` type also implements various
-/// traits such as `Clone`, `Debug`, `Hash`, `Eq`, `PartialEq`, `Ord`, and `PartialOrd`, making it
-/// easy to use and interact with in many contexts.
-///
-/// Note that while the `Sync<T>` type ensures that the inner value of the shared data can be
-/// safely accessed from multiple threads concurrently, it does not guarantee any particular order
-/// in which threads will access the data. If thread synchronization is required, additional
-/// mechanisms such as locks may need to be used in conjunction with `Sync<T>`.
-///
-/// ```rust
-/// # use imstr::data::{Data, Threadsafe};
-/// // Create a new `Threadsafe<String>` containing the value "Hello, world!".
-/// let mut shared = Threadsafe::new("Hello, world!".to_string());
-///
-/// // Use Data::get() to get a &String
-/// assert_eq!(shared.get(), "Hello, world!");
-///
-/// // Retrieve a mutable reference to the shared data using the `get_mut()` method and modify it.
-/// if let Some(string) = shared.get_mut() {
-///     string.push_str(" It's a beautiful day!");
-/// }
-///
-/// // This only worked because the string has not been cloned yet.
-/// assert_eq!(shared.get(), "Hello, world! It's a beautiful day!");
-///
-/// // Clone the shared data
-/// let cloned = shared.clone();
-/// assert_eq!(cloned, shared);
-///
-/// // Move the `Threadsafe<String>` to another thread and access the shared data concurrently.
-/// let handle = std::thread::spawn(move || {
-///     let mut cloned = cloned;
-///     assert_eq!(cloned.get(), "Hello, world! It's a beautiful day!");
-///     assert_eq!(cloned.get_mut(), None);
-/// });
-///
-/// // Wait for the other thread to complete.
-/// handle.join().unwrap();
-/// ```
-#[derive(Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
-pub struct Threadsafe<T>(Arc<T>);
-
-impl<T> Data<T> for Threadsafe<T> {
-    type Storage = Arc<T>;
-
+impl<T> Data<T> for Arc<T> {
     fn new(value: T) -> Self {
-        Threadsafe(Arc::new(value))
+        Arc::new(value)
     }
 
     fn get(&self) -> &T {
-        &self.0
+        &self
     }
 
     fn get_mut(&mut self) -> Option<&mut T> {
-        Arc::get_mut(&mut self.0)
-    }
-
-    fn raw(&self) -> Self::Storage {
-        self.0.clone()
+        Arc::get_mut(self)
     }
 }
 
-#[derive(Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
-pub struct Local<T>(Rc<T>);
-
-impl<T> Data<T> for Local<T> {
-    type Storage = Rc<T>;
-
+impl<T> Data<T> for Rc<T> {
     fn new(value: T) -> Self {
-        Local(Rc::new(value))
+        Rc::new(value)
     }
 
     fn get(&self) -> &T {
-        &self.0
+        &self
     }
 
     fn get_mut(&mut self) -> Option<&mut T> {
-        Rc::get_mut(&mut self.0)
-    }
-
-    fn raw(&self) -> Self::Storage {
-        self.0.clone()
+        Rc::get_mut(self)
     }
 }
 
-#[derive(Clone, Debug, Copy, Hash, Eq, PartialEq, Ord, PartialOrd)]
+impl<T: Clone> Data<T> for Box<T> {
+    fn new(value: T) -> Self {
+        Box::new(value)
+    }
+
+    fn get(&self) -> &T {
+        &self
+    }
+
+    fn get_mut(&mut self) -> Option<&mut T> {
+        Some(self)
+    }
+}
+
+/// Data which is simply cloned.
+#[derive(Clone)]
 pub struct Cloned<T>(T);
 
 impl<T: Clone> Data<T> for Cloned<T> {
-    type Storage = T;
-
     fn new(value: T) -> Self {
         Cloned(value)
     }
@@ -210,8 +166,41 @@ impl<T: Clone> Data<T> for Cloned<T> {
     fn get_mut(&mut self) -> Option<&mut T> {
         Some(&mut self.0)
     }
+}
 
-    fn raw(&self) -> Self::Storage {
-        self.0.clone()
+#[cfg(test)]
+fn test_i32<T: Data<i32> + Clone>() {
+    let mut number = T::new(16);
+    assert_eq!(number.get(), &16);
+    if let Some(number) = number.get_mut() {
+        *number += 4;
     }
+    assert_eq!(number.get(), &20);
+    let clone = number.clone();
+    assert_eq!(clone.get(), number.get());
+}
+
+#[cfg(test)]
+fn test_string<T: Data<String> + Clone>() {
+    let mut string = T::new("Hello".into());
+    assert_eq!(string.get(), "Hello");
+    if let Some(string) = string.get_mut() {
+        string.push_str(", World!");
+    }
+    assert_eq!(string.get(), "Hello, World!");
+    let clone = string.clone();
+    assert_eq!(clone.get(), string.get());
+}
+
+#[test]
+fn test_all_i32() {
+    test_i32::<Cloned<i32>>();
+    test_i32::<Arc<i32>>();
+    test_i32::<Rc<i32>>();
+    test_i32::<Box<i32>>();
+
+    test_string::<Cloned<String>>();
+    test_string::<Arc<String>>();
+    test_string::<Rc<String>>();
+    test_string::<Box<String>>();
 }
